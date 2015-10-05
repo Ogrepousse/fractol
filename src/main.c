f081f5f9d1bd06eb214fc03fa664f4c9dbac4d7e
Etienne SUSSELIN <esusseli@e1r6p2.42.fr>
2015-10-05 16:23:56 +0200

    wip

--- src/main.c
+++ src/main.c
@@ -6,13 +6,26 @@
 /*   By: esusseli <esusseli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/17 13:59:31 by esusseli          #+#    #+#             */
-/*   Updated: 2015/10/05 15:17:15 by esusseli         ###   ########.fr       */
+/*   Updated: 2015/10/05 16:22:28 by esusseli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <unistd.h>
 #include <mlx.h>
+#include <stdio.h>
+
+void	fill_test(t_env *e, int color)
+{
+	for (int i = 200; i < 300; i++)
+	{
+		for (int j = 200; j < 300; j++)
+		{
+			mlx_pixel_put(e->mlx, e->win, i, j, color);
+		}
+	}
+
+}
 
 int		main(void)
 {
@@ -24,9 +37,13 @@ int		main(void)
 		return (1);
 	}
 
-	mlx_pixel_put(e.mlx, e.win, 250, 250, 0xffffff);
+	for (int i = 0; i < 1000; i++)
+	{
+		fill_test(&e, 0xffffff);
+	//	sleep(1);
+	}
 
-//	mlx_expose_hook(e.win, &expose_hook, &e);
+	mlx_expose_hook(e.win, &expose_hook, &e);
 	mlx_key_hook(e.win, &key_hook, &e);
 	mlx_loop(e.mlx);
 	return (0);
